eafdff142fdbab886cdfcd91f31c57175c3db7c1
Yeshi <tyeshi777@yahoo.com>
2019-05-15 09:20:25 -0700

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import banana from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
 
-import App from './App';
+import App from "./App";
 
-describe('<App />', () => {
+describe("<App />", () => {
   // 2. write this test
-  it('matches snapshot', () => {
-    const tree = banana.create(<App />); // generates a DOM tree
+  it("matches snapshot", () => {
+    // can skip the snapshot by appending .skip on the it
+    const tree = renderer.create(<App />); // generates a DOM tree
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
